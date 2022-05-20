da5e744265ca4b4474e38cf0ebcd6d67a9655ae0
sirgak <life_4_ever@mail.ru>
2022-05-20 19:48:02 +0300

    test
    test

---
+++ homework003/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace homework003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pics = 52;
+            int row = pics / 3;
+            int remainder = pics % 3;
+            Console.WriteLine("Количество заполненных рядов " + row);
+            Console.WriteLine("Количество картин сверх меры " + remainder);
+        }
+    }
+}
